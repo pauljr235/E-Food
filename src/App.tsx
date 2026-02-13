2cc9c3412537c2e71d2809f82226d8fde0fe7c02
Paulo Junior <148215124+pauljr235@users.noreply.github.com>
2026-02-13 09:01:36 +0000

    Configuracao inicial e Global Style

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
-import { BrowserRouter } from 'react-router-dom'
 import { GlobalCss } from './styles'
-import Footer from './components/Footer'
-import Rotas from './routes'
+
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <GlobalCss />
-        <Rotas />
-        <Footer />
-      </BrowserRouter>
+      <GlobalCss />
     </>
   )
 }
--- src/styles.ts
+++ src/styles.ts
@@ -25,4 +25,4 @@ export const GlobalCss = createGlobalStyle`
 
   }
 `
-export const Container = styled
+
